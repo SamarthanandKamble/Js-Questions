f6504210968ea3f35cf9948f8f2f68bb5f39aa20
Samarth1002 <samarthanandkamble1002@gmail.com>
2024-04-21 23:08:50 +0530

    Leetcode Strings

--- PS/script.js
+++ PS/script.js
@@ -58,3 +58,26 @@ var defangIPaddr = function (address) {
 };
 
 */
+
+
+/*
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+
+var numJewelsInStones = function(jewels, stones) {
+    const tempArr = jewels.split("");
+    let count =0;
+    for(let i=0; i<stones.length; i++){
+        if(tempArr.includes(stones[i])) count++;
+    }
+return count;
+};
+*/
